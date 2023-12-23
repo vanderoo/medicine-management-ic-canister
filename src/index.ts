22b355735bb3e865055a82ab72043ef8461da909
Muhindo Galien <74584848+Muhindo-Galien@users.noreply.github.com>
2023-12-23 19:47:43 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,17 @@
 import { Canister, query, text, update, Record, Vec, Void } from 'azle';
 import { v4 as uuidv4 } from 'uuid';
 
-type Medicine = {
+type Medicine = Record<{
     id: string;
     name: string;
     quantity: string;
     expirationDate: string;
-};
+}>;
 
 let medicines: Medicine[] = [];
 
 export default Canister({
-    getMedicineInfo: query([text], Vec(Record({
-        "id": text,
-        "name": text,
-        "quantity": text,
-        "expirationDate": text
-    })), (medicineName) => {
+    getMedicineInfo: query([text], Vec(Medicine), (medicineName) => {
         const medicine = medicines.find((med) => med.name === medicineName);
         return medicine ? [medicine] : [];
     }),
@@ -34,41 +29,34 @@ export default Canister({
         }
     }),
 
-
-    getAllMedicines: query([], Vec(Record({
-        "id": text,
-        "name": text,
-        "quantity": text,
-        "expirationDate": text
-    })), () => medicines),
+    getAllMedicines: query([], Vec(Medicine), () => medicines),
 
     updateMedicineQuantity: update([text, text, text], Void, (name, soldQuantity, expirationDate) => {
         const medicine = medicines.find((med) => med.name === name && med.expirationDate === expirationDate);
       
         if (medicine) {
-          const currentQuantity = parseInt(medicine.quantity);
-          const soldQuantityInt = parseInt(soldQuantity);
-      
-          if (currentQuantity >= soldQuantityInt && currentQuantity - soldQuantityInt >= 0) {
-            medicine.quantity = (currentQuantity - soldQuantityInt).toString();
-            console.log(`Updated quantity for ${name} by selling ${soldQuantity}. Remaining quantity: ${medicine.quantity}`);
-            
-            if (medicine.quantity === '0') {
-              const medicineIndex = medicines.findIndex((med) => med.name === name && med.expirationDate === expirationDate);
-              if (medicineIndex !== -1) {
-                medicines.splice(medicineIndex, 1);
-                console.log(`Medicine ${name} with expiration date ${expirationDate} removed as quantity becomes 0`);
-              }
+            const currentQuantity = parseInt(medicine.quantity);
+            const soldQuantityInt = parseInt(soldQuantity);
+          
+            if (currentQuantity >= soldQuantityInt) {
+                medicine.quantity = (currentQuantity - soldQuantityInt).toString();
+                console.log(`Updated quantity for ${name} by selling ${soldQuantity}. Remaining quantity: ${medicine.quantity}`);
+                
+                if (medicine.quantity === '0') {
+                    const medicineIndex = medicines.findIndex((med) => med.name === name && med.expirationDate === expirationDate);
+                    if (medicineIndex !== -1) {
+                        medicines.splice(medicineIndex, 1);
+                        console.log(`Medicine ${name} with expiration date ${expirationDate} removed as quantity becomes 0`);
+                    }
+                }
+            } else {
+                console.log(`Cannot update quantity for ${name} (${expirationDate}). Invalid sold quantity.`);
             }
-          } else {
-            console.log(`Cannot update quantity for ${name} (${expirationDate}). Invalid sold quantity.`);
-          }
         } else {
-          console.log(`Medicine not found with name ${name} and expiration date ${expirationDate}`);
+            console.log(`Medicine not found with name ${name} and expiration date ${expirationDate}`);
         }
-      }),
-      
-
+    }),
+    
     removeExpiredMedicines: update([], Void, () => {
         medicines = medicines.filter((med) => new Date(med.expirationDate) > new Date());
         console.log('Removed expired medicines');
